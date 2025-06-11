b5a17648b856d2a1ecc534ed4e20bff5470b0efc
jeevan2436 <abelaaronmathewa@gmail.com>
2025-06-11 21:43:35 +0530

    failed push test

--- agents/Test_generator/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-PROJECT_ID = os.getenv("VERTEX_PROJECT_ID")
-LOCATION = os.getenv("VERTEX_LOCATION")
---
+++ wow.py
@@ -0,0 +1,1 @@
+print("pushes failed code")
